88f5cae0630db6ace453aad51d80875c49baaf8f
rafaellima02 <rafinhakazu123@gmail.com>
2026-07-17 09:34:24 -0300

    Final project update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="/home.css" />
 </svelte:head>
 
 <section>
@@ -69,6 +70,4 @@
   </div>
 </section>
 
-<style>
-  @import './home.css';
-</style>
+
